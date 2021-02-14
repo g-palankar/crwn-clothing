aeb5bccad1911a69b5b9e3013e266e744167fd13
Ganesh <ganeshpalankar01@gmail.com>
2021-02-14 23:36:02 +0530

    added routing - react-router-dom, added paths inside menu-item using withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = () => (<div>
+  <h1>The Hats page</h1>
+</div>)
+
 function App() {
   return (
-    <div><HomePage /></div>
+    <div>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route exact path='/hats' component={Hats}></Route>
+    </div>
   );
 }
 
